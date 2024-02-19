ec2cccb3302e723d470cc7035ccbcc79bca4f77d
LailaCharaoui <mats2@live.fr>
2024-02-20 00:49:09 +0100

    feat :sparkles: add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Card;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $object = (new User())
+                ->setUuid('uuid' . $i)
+                ->setRoles(['ROLE_USER'])
+                ->setPassword('password' . $i);
+            $manager->persist($object);
+
+            $object = (new Card())
+                ->setQuestion('question' . $i)
+                ->setAnswer('answer' . $i)
+                ->setTag('tag' . $i)
+                ->setCategory("FIRST")
+                ->setUser($object);
+
+
+            $manager->persist($object);
+
+        }
+
+
+        $manager->flush();
+    }
+
+
+
+}
